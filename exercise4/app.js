7f33210cb1c682138d550319f2c15c7cd81eb489
Ryan Zhou <ryanzhou342@gmail.com>
2023-05-04 17:30:21 -0300

    Exercise 4.12

---
+++ exercise4/app.js
@@ -0,0 +1,15 @@
+const cors = require("cors");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
+require("dotenv").config();
+
+mongoose.connect(config.MONGODB_URI);
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
