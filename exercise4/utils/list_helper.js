f982ffc8bcec46cfd9d09c8692088e75e4b4e3fa
Ryan Zhou <ryanzhou342@gmail.com>
2023-05-04 22:56:54 -0300

    Exercise 4.7

---
+++ exercise4/utils/list_helper.js
@@ -0,0 +1,14 @@
+const dummy = (blogs) => {
+  return 1;
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, current) => {
+    return sum + current.likes;
+  }, 0);
+};
+
+module.exports = {
+  dummy,
+  totalLikes
+};
