34aa38e9444033524d8f048a2b0deb6b674d7500
Ryan Zhou <ryanzhou342@gmail.com>
2023-05-24 16:51:28 -0400

    Exercise 5.13

---
+++ exercise5/src/App.js
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from "./services/login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [newBlog, setNewBlog] = useState({
+    title: "",
+    author: "",
+    url: ""
+  });
+
+  useEffect(() => {
+    blogService.getAll().then(blogs => {
+      setBlogs(blogs)
+    }); 
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
+    const user = await loginService.login({
+      username, password
+    });
+
+    window.localStorage.setItem(
+      "loggedBlogappUser", JSON.stringify(user)
+    );
+
+    setUser(user);
+    blogService.setToken(user.token);
+    setUsername("");
+    setPassword("");
+  };
+
+  const handleLogout = async (event) => {
+    event.preventDefault();
+
+    window.localStorage.removeItem("loggedBlogappUser");
+    setUser(null);
+  };
+
+  const handleCreate = async (event) => {
+    event.preventDefault();
+
+    const createdBlog = await blogService.create(newBlog);
+    setBlogs(blogs.concat(createdBlog));
+    setNewBlog({
+      title: "",
+      author: "",
+      url: ""
+    });
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)}/>
+          </div>
+          <div>
+            password
+              <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)}/>
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      <button type="submit" onClick={handleLogout}>logout</button>
+
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          title:
+            <input type="text" value={newBlog.title} name="title" onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })} />
+        </div>
+        <div>
+          author:
+            <input type="text" value={newBlog.author} name="author" onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })} />
+        </div>
+        <div>
+          url:
+            <input type="text" value={newBlog.url} name="url" onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
