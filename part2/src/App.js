c6ed1da53f6f542eb81fbc3e869566058450629d
Ryan Zhou <ryanzhou342@gmail.com>
2023-05-24 10:10:42 -0400

    Exercise 5.2

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import noteService from './services/notes';
 import Note from './components/Note';
 import Footer from './components/Footer';
 import Notification from './components/Notification';
+import loginService from "./services/login";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername]  = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
   
   useEffect(() => {
     noteService
@@ -18,6 +21,39 @@ const App = () => {
         setNotes(initialNotes);
       })
   }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+  
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      });
+      
+      window.localStorage.setItem(
+        "loggedNoteappUser", JSON.stringify(user)
+      );
+
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
 
   const addNote = (event) => {
     event.preventDefault();
@@ -54,16 +90,48 @@ const App = () => {
         setTimeout(() => {
           setErrorMessage(null);
         }, 5000)
-        setNotes(notes.filter(n => n.id != id));
+        setNotes(notes.filter(n => n.id !== id));
       })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
+  const loginForm = () => {
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        password
+          <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  };
+
+  const noteForm = () => {
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleChange} />
+      <button type="submit">save</button>
+    </form>
+  };
+
   return (
     <div>
       <h1>Notes</h1>
+
       <Notification message={errorMessage} />
+      
+      {user === null && loginForm()}
+      {user && <div>
+        <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+      }
+
+      <h2>Notes</h2>
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -74,10 +142,6 @@ const App = () => {
           <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleChange} />
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </div>
   )
