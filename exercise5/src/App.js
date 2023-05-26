894522c22580061685fdd2ced17d4dfcd5b3aa35
Ryan Zhou <ryanzhou342@gmail.com>
2023-05-26 12:51:29 -0400

    Exercise 5.7

--- exercise5/src/App.js
+++ exercise5/src/App.js
@@ -1,20 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
 import loginService from "./services/login";
 import Notification from "./components/Notification";
+import BlogForm from "./components/BlogForm";
+import Togglable from "./components/Togglable";
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
-  const [newBlog, setNewBlog] = useState({
-    title: "",
-    author: "",
-    url: ""
-  });
   const [message, setMessage] = useState("");
+  const blogFormRef = useRef();
 
   useEffect(() => {
     blogService.getAll().then(blogs => {
@@ -62,20 +60,14 @@ const App = () => {
     setUser(null);
   };
 
-  const handleCreate = async (event) => {
-    event.preventDefault();
-
+  const createBlog = async (newBlog) => {
+    blogFormRef.current.toggleVisibility();
     const createdBlog = await blogService.create(newBlog);
     setBlogs(blogs.concat(createdBlog));
-    setNewBlog({
-      title: "",
-      author: "",
-      url: ""
-    });
     setMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`);
     setTimeout(() => {
       setMessage(null);
-    }, 5000);
+    }, 5000)
   };
 
   if (user === null) {
@@ -104,23 +96,9 @@ const App = () => {
       <Notification message={message} />
       <p>{user.name} logged in</p>
       <button type="submit" onClick={handleLogout}>logout</button>
-
-      <h2>create new</h2>
-      <form onSubmit={handleCreate}>
-        <div>
-          title:
-            <input type="text" value={newBlog.title} name="title" onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })} />
-        </div>
-        <div>
-          author:
-            <input type="text" value={newBlog.author} name="author" onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })} />
-        </div>
-        <div>
-          url:
-            <input type="text" value={newBlog.url} name="url" onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })} />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <Togglable buttonLabel="new blog" ref={blogFormRef}>
+        <BlogForm createBlog={createBlog} />
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ exercise5/src/components/BlogForm.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const BlogForm = ({ createBlog }) => {
+  const [newBlog, setNewBlog] = useState({
+    title: "",
+    author: "",
+    url: ""
+  });
+
+  const addBlog = (event) => {
+    event.preventDefault();
+
+    createBlog(newBlog);
+
+    setNewBlog({
+      title: "",
+      author: "",
+      url: ""
+    });
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+            <input type="text" value={newBlog.title} name="title" onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })} />
+        </div>
+        <div>
+          author:
+            <input type="text" value={newBlog.author} name="author" onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })} />
+        </div>
+        <div>
+          url:
+            <input type="text" value={newBlog.url} name="url" onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
